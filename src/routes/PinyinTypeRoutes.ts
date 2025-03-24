3e2cf76632c06dbdafe81a3860bf6acda702d597
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-24 17:03:57 +1100

    moved location of database

--- src/routes/PinyinTypeRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getCharacters } from "../controllers/PinyinTypeController.js";
-
-const router = Router();
-
-router.get("/characters", getCharacters);
-
-export default router;
